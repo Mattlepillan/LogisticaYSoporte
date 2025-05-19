5239fff8cb80219bce66f892dd0b8c10b00f5b04
Matthew <ma.lepillan@duocuc.cl>
2025-05-19 10:31:20 -0400

    first commit

---
+++ src/main/java/com/Microservicio/LogisticaYSoporte/LogisticaYSoporteApplication.java
@@ -0,0 +1,13 @@
+package com.Microservicio.LogisticaYSoporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticaYSoporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticaYSoporteApplication.class, args);
+	}
+
+}
