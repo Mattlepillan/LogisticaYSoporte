ac71c0f828771fc847be5de9f0453989ffc69c87
Matthew <ma.lepillan@duocuc.cl>
2025-05-19 10:41:41 -0400

    Nuevo commit

---
+++ src/main/java/com/Microservicio/LogisticaYSoporte/model/Incidencia.java
@@ -0,0 +1,31 @@
+package com.Microservicio.LogisticaYSoporte.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cursos")
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Incidencia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 500, nullable = false)
+    private String descripcion;
+
+    @Column(length = 100, nullable = false)
+    private String estado;
+    
+}
